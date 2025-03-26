6db939f25617512148089916247b0fa519a39240
Daniel <daniel.mutembei@student.moringaschool.com>
2025-03-26 20:05:45 +0300

    “done”

--- server/models.py
+++ server/models.py
@@ -11,14 +11,50 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 
+class Review(db.Model):
+    __tablename__ = 'reviews'
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
+    customer = db.relationship('Customer', back_populates='reviews')
+    item = db.relationship('Item', back_populates='reviews')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'comment': self.comment,
+            'customer_id': self.customer_id,
+            'item_id': self.item_id,
+            'customer': {  # Include customer as a dictionary with just the id and name
+                'id': self.customer.id if self.customer else None,
+                'name': self.customer.name if self.customer else None
+            },
+            'item': {  # Include item as a dictionary with just the id and name
+                'id': self.item.id if self.item else None,
+                'name': self.item.name if self.item else None,
+                'price': self.item.price if self.item else None
+        }
+    }
+
+
 class Customer(db.Model):
     __tablename__ = 'customers'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+    reviews = db.relationship('Review', back_populates='customer')
+    items = association_proxy('reviews', 'item')
 
     def __repr__(self):
         return f'<Customer {self.id}, {self.name}>'
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'reviews': [review.id for review in self.reviews]  # Include only review IDs to avoid recursion
+        }
 
 
 class Item(db.Model):
@@ -27,6 +63,15 @@ class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     price = db.Column(db.Float)
+    reviews = db.relationship('Review', back_populates='item')
 
     def __repr__(self):
         return f'<Item {self.id}, {self.name}, {self.price}>'
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'price': self.price,
+            'reviews': [review.id for review in self.reviews]  # Include only review IDs to avoid recursion
+        }
